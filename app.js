d3d7c745024953c600f4fee4af97e705ee989537
manogna1608 <komaturimanogna@gmail.com>
2024-10-22 15:33:54 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ const app = express();
 app.get('/',(req,res) => { 
 res.send("Hello World, I am from Nodejs"); 
 }); 
-app.listen(3060,() => { 
+app.listen(3070,() => { 
 console.log("Server is running on port 3060") 
 });
